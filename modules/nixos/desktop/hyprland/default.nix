1ec46aeeea3ffa61eb64efbd6680a708cd7b3ac0
Huayu Wang <why123jj@126.com>
2024-09-01 21:47:36 +0800

    feat: write basic hyprland conf

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -9,6 +9,22 @@ in {
 
   config = mkIf cfg.enable {
     programs.hyprland.enable = true;
+    services.gnome.gnome-keyring.enable = true;
+    environment = {
+      # plasma5.excludePackages = [ pkgs.kdePackages.systemsettings ];
+      # plasma6.excludePackages = [ pkgs.kdePackages.systemsettings ];
+    };
+
+    services.xserver = {
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+        enableHidpi = true;
+        # theme = "chili";
+        # package = pkgs.sddm;
+      };
+
+    };
 
     #== Home Manager
     literacy.home = {
@@ -69,6 +85,50 @@ in {
                     1.236; # default split ratio for new windows
                 };
 
+                #== Workspaces
+                workspace = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
+                #== Key bindings
+                "$mod" = "SUPER";
+                bind = [
+                  #== Basic window manipulation
+                  "$mainMod, T, togglefloating" # toggle the window on focus to float
+                  "$mainMod, F, fullscreen"
+                  "$mainMod, n, movefocus, l"
+                  "$mainMod, i, movefocus, r"
+                  "$mainMod, u, movefocus, u"
+                  "$mainMod, e, movefocus, d"
+
+                  "$mainMod_SHIFT, I, swapwindow, r"
+                  "$mainMod_SHIFT, N, swapwindow, l"
+                  "$mainMod_SHIFT, E, swapwindow, d"
+                  "$mainMod_SHIFT, U, swapwindow, u"
+
+                  # Scroll through existing workspaces with mainMod + scroll
+                  "$mainMod, mouse_down, workspace, e+1"
+                  "$mainMod, mouse_up, workspace, e-1"
+
+                ] ++ (
+                  #== Workspaces
+                  # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+                  builtins.concatLists (builtins.genList (i:
+                    let ws = i + 1;
+                    in [
+                      "$mod, code:1${toString i}, workspace, ${toString ws}"
+                      "$mod SHIFT, code:1${toString i}, movetoworkspace, ${
+                        toString ws
+                      }"
+                    ]) 9));
+
+                #== Mouse bindings
+                bindm = [
+                  # Move/resize windows with mainMod + LMB/RMB and dragging
+                  "$mainMod, mouse:272, movewindow"
+                  "$mainMod, mouse:273, resizewindow"
+                ];
+
+              };
+              extraConfig = {
+                #== Submaps
               };
 
               plugins = [ ];
@@ -76,6 +136,8 @@ in {
           };
         };
       };
+      packages = with pkgs; [ wl-clipboard networkmanagerapplet alsa-utils ];
     };
+
   };
 }
