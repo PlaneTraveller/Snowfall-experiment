30e2f4855b94453abd96d9d38cf931ce46f22deb
PlaneTraveller <why123jj@126.com>
2024-10-16 20:36:25 +0800

    feat: add github api token

--- homes/x86_64-linux/wanghy@yousatech-R48/default.nix
+++ homes/x86_64-linux/wanghy@yousatech-R48/default.nix
@@ -18,16 +18,32 @@ with lib.literacy; {
   };
   programs.ssh.enable = true;
   services.ssh-agent.enable = true;
-  programs.fish.enable = true;
+
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set -Ux OPENBLAS_NUM_THREADS 32
+    '';
+  };
+
+  nix.settings = {
+    access-tokens = "github.com=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";
+  };
 
   home.packages = with pkgs; [
     btop
     fzf
     unison
     python311Packages.python-lsp-server
+    tmux
+    pv
+    progress
+    lynx
+    ncdu
   ];
 
   # home.sessionPath = [ "$HOME/bin" ];
+
 
   home.stateVersion = "23.11";
 }
